4609362035a97e98c84481a84bca080421c6d294
ne5oo <106486790+neo5oo@users.noreply.github.com>
2023-12-07 13:44:22 +0300

    Инициализация проекта + базовый роутинг

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+function Header() {
+  return (
+    <nav className="indigo lighten-2">
+      <div className="nav-wrapper">
+        <a href="/" className="brand-logo">
+          React Shop
+        </a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li>
+            <a
+              href="https://neo5oo.github.io/react-shop/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              Repo
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export { Header };
