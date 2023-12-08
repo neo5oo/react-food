d65a470d7ffcbfb011dbaba8f3db2561a21ee728
ne5oo <106486790+neo5oo@users.noreply.github.com>
2023-12-08 14:24:36 +0300

    Страница рецепта + поиск

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-} from "react-router-dom/cjs/react-router-dom.min";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Contact } from "./pages/Contact";
+import { Category } from "./pages/Category";
 import { NotFound } from "./pages/NotFound";
-import { Movie } from "./pages/Movie";
+import { Recipe } from "./pages/Recipe";
 
 function App() {
   return (
     <>
-      <Router>
+      <Router basename="/react-food">
         <Header />
         <main className="container content">
           <Switch>
-            <Route exact path="/" component={Home} />
+            <Route exact path="/">
+              <Home />
+            </Route>
             <Route path="/about" component={About} />
             <Route path="/contacts" component={Contact} />
-            <Route path="/movies/:id" component={Movie} />
+            <Route path="/category/:name" component={Category} />
+            <Route path="/meal/:id" component={Recipe} />
             <Route component={NotFound} />
           </Switch>
         </main>
--- src/pages/Movie.jsx
+++
@@ -1,7 +0,0 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom";
-
-function Movie() {
-  return <h1>Some movie</h1>;
-}
-
-export { Movie };
